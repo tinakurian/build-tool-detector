f5e9d2bd3fdfbc57d052449acf1f0b9da814536e
tinakurian <tkurian@redhat.com>
2018-08-24 07:47:15 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+//go:generate goagen bootstrap -d build-tool-detector/design
+
+package main
+
+import (
+	"build-tool-detector/app"
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/middleware"
+)
+
+func main() {
+	// Create service
+	service := goa.New("build-tool-detector")
+
+	// Mount middleware
+	service.Use(middleware.RequestID())
+	service.Use(middleware.LogRequest(true))
+	service.Use(middleware.ErrorHandler(service, true))
+	service.Use(middleware.Recover())
+
+	// Mount "build-tool-detector" controller
+	c := NewBuildToolDetectorController(service)
+	app.MountBuildToolDetectorController(service, c)
+
+	cs := NewSwaggerController(service)
+	app.MountSwaggerController(service, cs)
+
+	// Start service
+	if err := service.ListenAndServe(":8080"); err != nil {
+		service.LogError("startup", "err", err)
+	}
+}
