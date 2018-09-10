606f24e56220310af973de89bab2cfe5b26b0a08
tinakurian <tkurian@redhat.com>
2018-09-10 11:27:57 -0400

    adding ginkgo

---
+++ controllers/build-tool-detector_test.go
@@ -0,0 +1,12 @@
+package controllers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "build-tool-detector/controllers"
+)
+
+var _ = Describe("BuildToolDetector", func() {
+
+})
