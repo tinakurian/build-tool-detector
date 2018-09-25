d82ad8af61d5cf37b8496dfd8b160d25cba9b9d2
tinakurian <tkurian@redhat.com>
2018-09-25 11:47:16 -0400

    fix comments for git package

--- controllers/git/git_service_type_test.go
+++ controllers/git/git_service_type_test.go
@@ -14,48 +14,48 @@ var _ = Describe("GitServiceType", func() {
 		It("Faulty Host - empty", func() {
 			serviceType, err := git.GetGitServiceType("")
 			Expect(serviceType).Should(BeNil(), "service type should be 'nil'")
-			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be 'nil'")
+			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be '400'")
 		})
 
 		It("Faulty Host - non-existent", func() {
 			serviceType, err := git.GetGitServiceType("test/test")
 			Expect(serviceType).Should(BeNil(), "service type should be 'nil'")
-			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be 'nil'")
+			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be '400'")
 		})
 
 		It("Faulty Host - not github.com", func() {
 			serviceType, err := git.GetGitServiceType("http://test.com/test/test")
 			Expect(serviceType).Should(BeNil(), "service type should be 'nil'")
-			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusInternalServerError), "service type should be 'nil'")
+			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusInternalServerError), "service type should be '500'")
 		})
 
 		It("Faulty url - no repository", func() {
 			serviceType, err := git.GetGitServiceType("http://github.com/test")
 			Expect(serviceType).Should(BeNil(), "service type should be 'nil'")
-			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be 'nil'")
+			Expect(err.StatusCode).Should(BeEquivalentTo(http.StatusBadRequest), "service type should be '400'")
 		})
 
 		It("Correct url - non-existent", func() {
 			serviceType, err := git.GetGitServiceType("http://github.com/test/test")
-			Expect(serviceType).ShouldNot(BeNil(), "service type should be 'nil'")
-			Expect(err).Should(BeNil(), "service type should be 'nil'")
+			Expect(serviceType).ShouldNot(BeNil(), "service type should be not be'nil'")
+			Expect(err).Should(BeNil(), "err should be 'nil'")
 		})
 	})
 
 	Context("Constants", func() {
 		It("Github", func() {
-			Expect(git.GITHUB).Should(BeEquivalentTo("github"), "service type should be 'nil'")
+			Expect(git.GITHUB).Should(BeEquivalentTo("github"), "git.GITHUB should be 'github'")
 		})
 
 		It("Unknown", func() {
-			Expect(git.UNKNOWN).Should(BeEquivalentTo("unknown"), "service type should be 'nil'")
+			Expect(git.UNKNOWN).Should(BeEquivalentTo("unknown"), "git.UNKNOWN should be 'unknown'")
 		})
 	})
 
 	Context("Service", func() {
 		It("Github", func() {
 			service := git.Service{}.GetGitHubService()
-			Expect(service).Should(BeEquivalentTo(&github.GitService{}), "service type should be 'nil'")
+			Expect(service).Should(BeEquivalentTo(&github.GitService{}), "service type should be a git service")
 		})
 	})
 })
