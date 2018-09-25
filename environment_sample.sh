d7e8e0e7eb784eba0894fdf258d295e9ae262241
tinakurian <tkurian@redhat.com>
2018-09-25 16:15:34 -0400

    adding environment.sh for environmnet variables

---
+++ environment_sample.sh
@@ -0,0 +1,21 @@
+#########################################################################
+#   
+# Exports environment variables required to use this service.
+# Currently, the service requires you to register the application. 
+# This can be done by following instructions 
+# found here: https://auth0.com/docs/connections/social/github 
+#
+# HOW TO:
+# --------
+#
+# To use this file copy the contents and create a new file titled
+# 'environment.sh' and populate the variables with the
+# appropriate values. Source the file like so: source ./environment.sh
+#
+#########################################################################
+
+# Export github client id
+export GITHUB_CLIENT_ID=""
+
+# Export github client secret
+export GITHUB_CLIENT_SECRET=""
