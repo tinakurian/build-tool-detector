54b21762365981144b4f6582963c04bfa12f5a15
tinakurian <tkurian@redhat.com>
2018-10-11 13:29:46 -0400

    use beforeeach and aftereach for tests

--- controllers/build-tool-detector_test.go
+++ controllers/build-tool-detector_test.go
@@ -20,9 +20,17 @@ import (
 
 var _ = Describe("BuildToolDetector", func() {
 	Context("Internal Server Error", func() {
-		It("Non-existent owner name -- 404 Repository Not Found", func() {
-			defer gock.Off()
+		var service *goa.Service
+		BeforeEach(func() {
+			service = goa.New("build-tool-detector")
+		})
 
+		AfterEach(func() {
+			gock.Off()
+		})
+		It("Non-existent owner name -- 404 Repository Not Found", func() {
+
+			// Fail instead of expect- and make this a method
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_launcher_backend/not_found_repo_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -31,14 +39,11 @@ var _ = Describe("BuildToolDetector", func() {
 				Reply(404).
 				BodyString(string(bodyString))
 
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			test.ShowBuildToolDetectorNotFound(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-launcherz/launcher-backend", &branch)
 		})
 
 		It("Non-existent owner name -- 404 Owner Not Found", func() {
-			defer gock.Off()
-
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_launcher_backend/not_found_repo_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -47,14 +52,11 @@ var _ = Describe("BuildToolDetector", func() {
 				Reply(404).
 				BodyString(string(bodyString))
 
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			test.ShowBuildToolDetectorNotFound(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-launcher/launcher-backendz", &branch)
 		})
 
 		It("Non-existent branch name -- 404 Branch Not Found", func() {
-			defer gock.Off()
-
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_launcher_backend/not_found_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -63,32 +65,34 @@ var _ = Describe("BuildToolDetector", func() {
 				Reply(404).
 				BodyString(string(bodyString))
 
-			service := goa.New("build-tool-detector")
 			test.ShowBuildToolDetectorNotFound(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-launcher/launcher-backend/tree/masterz", nil)
 		})
 
 		It("Invalid URL -- 400 Bad Request", func() {
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			test.ShowBuildToolDetectorBadRequest(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "fabric8-launcher/launcher-backend", &branch)
 		})
 
 		It("Unsupported Git Service -- 500 Internal Server Error", func() {
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			test.ShowBuildToolDetectorInternalServerError(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "http://gitlab.com/fabric8-launcher/launcher-backend", &branch)
 		})
 
 		It("Invalid URL and Branch -- 500 Internal Server Error", func() {
-			service := goa.New("build-tool-detector")
 			test.ShowBuildToolDetectorBadRequest(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "", nil)
 		})
 	})
 
 	Context("Okay", func() {
-		It("Recognize Unknown - Branch field populated", func() {
-			defer gock.Off()
+		var service *goa.Service
+		BeforeEach(func() {
+			service = goa.New("build-tool-detector")
+		})
 
+		AfterEach(func() {
+			gock.Off()
+		})
+		It("Recognize Unknown - Branch field populated", func() {
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_wit/ok_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -103,15 +107,12 @@ var _ = Describe("BuildToolDetector", func() {
 				Get("/repos/fabric8-services/fabric8-wit/contents/pom.xml").
 				Reply(404).
 				BodyString(string(bodyString))
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			_, buildTool := test.ShowBuildToolDetectorOK(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-services/fabric8-wit", &branch)
 			Expect(buildTool.BuildToolType).Should(Equal("unknown"), "buildTool should not be empty")
 		})
 
 		It("Recognize Unknown - Branch included in URL", func() {
-			defer gock.Off()
-
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_wit/ok_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -126,14 +127,11 @@ var _ = Describe("BuildToolDetector", func() {
 				Get("/repos/fabric8-services/fabric8-wit/contents/pom.xml").
 				Reply(404).
 				BodyString(string(bodyString))
-			service := goa.New("build-tool-detector")
 			_, buildTool := test.ShowBuildToolDetectorOK(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-services/fabric8-wit/tree/master", nil)
 			Expect(buildTool.BuildToolType).Should(Equal("unknown"), "buildTool should not be empty")
 		})
 
 		It("Recognize Maven - Branch field populated", func() {
-			defer gock.Off()
-
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_launcher_backend/ok_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -148,15 +146,12 @@ var _ = Describe("BuildToolDetector", func() {
 				Get("/repos/fabric8-launcher/launcher-backend/contents/pom.xml").
 				Reply(200).
 				BodyString(string(bodyString))
-			service := goa.New("build-tool-detector")
 			branch := "master"
 			_, buildTool := test.ShowBuildToolDetectorOK(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-launcher/launcher-backend", &branch)
 			Expect(buildTool.BuildToolType).Should(Equal("maven"), "buildTool should not be empty")
 		})
 
 		It("Recognize Maven - Branch included in URL", func() {
-			defer gock.Off()
-
 			bodyString, err := ioutil.ReadFile("../controllers/test/mock/fabric8_launcher_backend/ok_branch.json")
 			Expect(err).Should(BeNil())
 
@@ -171,7 +166,6 @@ var _ = Describe("BuildToolDetector", func() {
 				Get("/repos/fabric8-launcher/launcher-backend/contents/pom.xml").
 				Reply(200).
 				BodyString(string(bodyString))
-			service := goa.New("build-tool-detector")
 			_, buildTool := test.ShowBuildToolDetectorOK(GinkgoT(), nil, nil, controllers.NewBuildToolDetectorController(service, "test", "test"), "https://github.com/fabric8-launcher/launcher-backend/tree/master", nil)
 			Expect(buildTool.BuildToolType).Should(Equal("maven"), "buildTool should not be empty")
 		})
